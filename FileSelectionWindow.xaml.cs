14baeaca70187dddfe12f863b386dea6ef354c51
dninemfive <11084010+dninemfive@users.noreply.github.com>
2022-02-26 14:47:12 -0800

    file selection window

---
+++ FileSelectionWindow.xaml.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace bkp
+{
+    /// <summary>
+    /// Interaction logic for FileSelectionWindow.xaml
+    /// </summary>
+    public partial class FileSelectionWindow : Window
+    {
+        public FileSelectionWindow()
+        {
+            InitializeComponent();
+        }
+    }
+}
