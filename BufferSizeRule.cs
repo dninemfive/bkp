39061a4bffde67bc014e513d1fe7f167440ebd58
dninemfive <11084010+dninemfive@users.noreply.github.com>
2022-03-31 10:37:21 -0700

    tried doing this the correct way (stupid)

---
+++ BufferSizeRule.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Controls;
+
+namespace bkp
+{
+    /// <summary>
+    /// Validates that a string in a <c>TextBox</c> is a valid buffer size, i.e. an integer greater than 0.
+    /// </summary>
+    /// <remarks>Largely copied from https://docs.microsoft.com/en-us/dotnet/desktop/wpf/data/how-to-implement-binding-validation?view=netframeworkdesktop-4.8. </remarks>
+    public class BufferSizeRule : ValidationRule
+    {
+        public override ValidationResult Validate(object value, CultureInfo cultureInfo)
+        {
+            int result = default;
+            try
+            {
+                if (value is string s && s.Length > 0) result = int.Parse(s);
+            }
+            catch(Exception e)
+            {
+                return new ValidationResult(false, e.Message);
+            }
+            if (result < 1) return new ValidationResult(false, "Buffer size must be greater than zero.");
+            return ValidationResult.ValidResult;
+        }
+    }
+}
